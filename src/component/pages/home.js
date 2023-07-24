a69a4736124d443070d316f9da20e0f1a7e1191c
KhushiAgarwal20 <83128859+KhushiAgarwal20@users.noreply.github.com>
2023-07-24 19:09:21 +0530

    Update home.js

--- src/component/pages/home.js
+++ src/component/pages/home.js
@@ -9,71 +9,36 @@ import axios from 'axios';
 
 
 
-
 function Home() {
     const [selectedFile, setSelectedFile] = useState();
     const [isSelected, setIsFilePicked] = useState(false);
-    const [choose_upload, setupload] = useState("0");
     const [chunk, setchunk] = useState("");
     var _validFileExtensions = [".pdf", ".txt"];
 
-    const [text, setText] = useState();
-
     let fileReader;
+    const navigate = useNavigate();
 
     const onChange = e => {
         let file = e.target.files;
         fileReader = new FileReader();
         fileReader.onloadend = handleFileRead;
         fileReader.readAsText(file[0]);
-        console.log(text);
     };
 
-    const cleanContent = string => {
+    const createChunks = string => {
         string = string.replace(/^\s*[\r\n]/gm, "");
         let array = string.split(new RegExp(/[\r\n]/gm));
-        console.log(array);
-        array.splice(0, 3);
-        array.splice(-3);
-        return array.join("\n");
+        // console.log(array);
+        return array;
     };
 
     const handleFileRead = e => {
         let content = fileReader.result;
-        // let text = deleteLines(content, 3);
-        content = cleanContent(content);
+        content = createChunks(content);
+        console.log(content);
         // … do something with the 'content' …
-        setText(content);
+        setchunk(content);
     };
-
-
-    const navigate = useNavigate();
-
-    function createChunks(chunkSize, text) {
-        const chunks = [];
-
-        const words = text.split('\n');
-
-        for (let i = 0; i < words.length; i++) {
-            chunks.push(words[i]);
-        }
-
-
-
-        // for (let i = 0; i < text.length; i += chunkSize) {
-        //     chunks.push(text.substring(i, i + chunkSize));
-        // }
-        return chunks;
-    };
-
-    function handleFileLoad(event) {
-        const content = event.target.result;
-        const contentString = String.fromCharCode.apply(null, new Uint8Array(content));
-
-        const chunks = createChunks(70, contentString);
-        console.log(chunks)
-        setchunk(chunks);
-    }
 
     async function PushChunks(chunk, i, filename) {
         axios.post('https://datacollection-qrgp.onrender.com/user/addText', {
@@ -111,14 +76,15 @@ function Home() {
         await AlertHandler(k, chunk.length);
     };
     const ValidateSingleInput = (oInput) => {
+        let Input_file = oInput;
         oInput = oInput.target;
-        if (oInput.type == "file") {
+        if (oInput.type === "file") {
             var sFileName = oInput.value;
             if (sFileName.length > 0) {
                 var blnValid = false;
                 for (var j = 0; j < _validFileExtensions.length; j++) {
                     var sCurExtension = _validFileExtensions[j];
-                    if (sFileName.substr(sFileName.length - sCurExtension.length, sCurExtension.length).toLowerCase() == sCurExtension.toLowerCase()) {
+                    if (sFileName.substr(sFileName.length - sCurExtension.length, sCurExtension.length).toLowerCase() === sCurExtension.toLowerCase()) {
                         blnValid = true;
                         break;
                     }
@@ -130,25 +96,21 @@ function Home() {
                 }
                 setSelectedFile(oInput.files[0]);
                 setIsFilePicked(true);
-                const file = oInput.files[0];
-                const reader = new FileReader();
-                reader.onload = handleFileLoad;
-                reader.readAsArrayBuffer(file);
+                onChange(Input_file);
             }
         }
         return true;
     };
     return (
-        <div style={{ display: 'flex', flexDirection: 'row', background: 'white' }}>
+        <div style={{display:'flex',flexDirection:'row',background:'white'}}>
             <Sidebar />
-            <div style={{ display: 'flex', flexDirection: 'column', background: ' rgb(239, 236, 236)', justifyContent: 'center', alignItems: 'center', width: '100%', }}>
+            <div style={{display:'flex',flexDirection:'column',background:' rgb(239, 236, 236)',justifyContent:'center',alignItems:'center',width:'100%', }}>
                 <div >
                     <div className='home_card'>
                         <br />
 
                         {/* <input type="file" name="file" onChange={changeHandler} /> */}
-                        {/* <input type="file" name="file" onChange={ValidateSingleInput} /> */}
-                        <input type="file" name="myfile" onChange={onChange} />
+                        <input type="file" name="file" onChange={ValidateSingleInput} />
 
                         {isSelected ? (
                             //display error msg if the file uploaded is not .txt/.pdf
@@ -157,8 +119,8 @@ function Home() {
                             <div>
                                 <br />
                                 <p> Filename: {selectedFile.name}</p>
-                                <p><t></t>Filetype: {selectedFile.type}</p>
-                                <p><t></t>Size in bytes: {selectedFile.size}</p>
+                                <p>Filetype: {selectedFile.type}</p>
+                                <p>Size in bytes: {selectedFile.size}</p>
                                 <p>
                                     last Modified Date:{' '}
                                     {selectedFile.lastModifiedDate.toLocaleDateString()}
